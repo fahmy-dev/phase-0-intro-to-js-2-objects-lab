65d5709bb46f575a73c534d6440429fdbbf837aa
fahmy-dev <faahmy@proton.me>
2024-12-01 12:16:21 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name: "Fahmy",
+    streetAddress: "SouthCity",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = {...employee};
+    newemployee[key] = value;
+    return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const delemployee = {...employee};
+    delete delemployee[key];
+    return delemployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
